096b9ba4cf051fe35b4a2ef598b455411e751b5a
madhu179 <madhubabu232343@gmail.com>
2020-10-06 18:42:43 +0530

    Commiting Welcome program

---
+++ src/com/capgemini/employeewagebuilder/EmployeeWageBuilder.java
@@ -0,0 +1,9 @@
+package com.capgemini.employeewagebuilder;
+
+public class EmployeeWageBuilder {
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to Employee Wage Computation Program");
+	}
+
+}
