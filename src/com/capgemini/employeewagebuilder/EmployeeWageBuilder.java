6c2f2fedddecc761d37c032822b31bf7fe86a82b
madhu179 <madhubabu232343@gmail.com>
2020-10-06 18:48:26 +0530

    Commiting Use Case 1 Files

--- src/com/capgemini/employeewagebuilder/EmployeeWageBuilder.java
+++ src/com/capgemini/employeewagebuilder/EmployeeWageBuilder.java
@@ -2,8 +2,16 @@ package com.capgemini.employeewagebuilder;
 
 public class EmployeeWageBuilder {
 	public static void main(String[] args) {
+		final int PRESENT = 1;
+		double employeeCheck = Math.floor(Math.random() * 10) % 2;
+		if (employeeCheck == PRESENT) {
+			System.out.println("Employee is present");
+		}
 
-		System.out.println("Welcome to Employee Wage Computation Program");
+		else {
+			System.out.println("Employee is absent");
+		}
+
 	}
 
 }
