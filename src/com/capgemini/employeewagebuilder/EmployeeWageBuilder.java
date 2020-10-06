786f5f972612b844c0604f3a2d8ffd470f8c99dd
madhu179 <madhubabu232343@gmail.com>
2020-10-06 19:04:32 +0530

    Commiting Use Case 4 Files

--- src/com/capgemini/employeewagebuilder/EmployeeWageBuilder.java
+++ src/com/capgemini/employeewagebuilder/EmployeeWageBuilder.java
@@ -1,25 +1,27 @@
 package com.capgemini.employeewagebuilder;
 
 public class EmployeeWageBuilder {
+
+	public static final int IS_FULL_TIME = 1;
+	public static final int IS_PART_TIME = 2;
+
 	public static void main(String[] args) {
-		final int IS_FULL_TIME = 1;
-		final int IS_PART_TIME = 2;
+
 		final int RATE_PER_HR = 20;
 		int noOfHrs = 0;
 		int wage = 0;
 		double employeeCheck = Math.floor(Math.random() * 10) % 3;
-		if (employeeCheck == IS_FULL_TIME) {
+		switch ((int) employeeCheck) {
+		case IS_FULL_TIME:
 			noOfHrs = 8;
-		}
-
-		else if (employeeCheck == IS_PART_TIME) {
+			break;
+		case IS_PART_TIME:
 			noOfHrs = 4;
-		}
-
-		else {
+			break;
+		default:
 			noOfHrs = 0;
+			break;
 		}
-
 		wage = noOfHrs * RATE_PER_HR;
 		System.out.println("Employee wage is : " + wage);
 
