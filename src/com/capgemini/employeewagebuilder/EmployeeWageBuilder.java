d9972cad22c5e2085d137a8dbdbd96990b29e3ec
madhu179 <madhubabu232343@gmail.com>
2020-10-06 19:11:20 +0530

    Commiting Use case 5 Files

--- src/com/capgemini/employeewagebuilder/EmployeeWageBuilder.java
+++ src/com/capgemini/employeewagebuilder/EmployeeWageBuilder.java
@@ -4,27 +4,33 @@ public class EmployeeWageBuilder {
 
 	public static final int IS_FULL_TIME = 1;
 	public static final int IS_PART_TIME = 2;
+	public static final int noOfWorkDays = 20;
 
 	public static void main(String[] args) {
 
 		final int RATE_PER_HR = 20;
 		int noOfHrs = 0;
 		int wage = 0;
-		double employeeCheck = Math.floor(Math.random() * 10) % 3;
-		switch ((int) employeeCheck) {
-		case IS_FULL_TIME:
-			noOfHrs = 8;
-			break;
-		case IS_PART_TIME:
-			noOfHrs = 4;
-			break;
-		default:
-			noOfHrs = 0;
-			break;
-		}
-		wage = noOfHrs * RATE_PER_HR;
-		System.out.println("Employee wage is : " + wage);
+		int totalEmpWage = 0;
 
+		for (int d = 0; d < noOfWorkDays; d++) {
+			double employeeCheck = Math.floor(Math.random() * 10) % 3;
+			switch ((int) employeeCheck) {
+			case IS_FULL_TIME:
+				noOfHrs = 8;
+				break;
+			case IS_PART_TIME:
+				noOfHrs = 4;
+				break;
+			default:
+				noOfHrs = 0;
+				break;
+			}
+			wage = noOfHrs * RATE_PER_HR;
+			totalEmpWage = totalEmpWage + wage;
+			System.out.println("Employee wage is : " + wage);
+		}
+		System.out.println("Total Employee wage is : " + totalEmpWage);
 	}
 
 }
